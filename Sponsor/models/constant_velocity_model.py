ad57a71bb54693e4711e865763ca700bb6fc90b7
alro9829 <70238044+alro9829@users.noreply.github.com>
2022-10-27 00:25:23 -0600

    fixing alpha-beta filter

--- Sponsor/models/constant_velocity_model.py
+++ Sponsor/models/constant_velocity_model.py
@@ -1,5 +1,6 @@
 
 import matplotlib.pyplot as plt
+import numpy as np
 import time
 
 class ConstantVelocityModel:
@@ -19,35 +20,37 @@ class ConstantVelocityModel:
         self.measured_values.append(value)
 
     def AB_filter(self, iterations):
-
-        fig = plt.figure()
-        fig.show()
-        plt.axis([0, 20, 0, 50000])
-
         prev_pos = self.initial_position + (self.unit_time * self.initial_velocity)
         prev_vel = self.initial_velocity
-
+        positions = []
+        velocities = []
         for i in range(iterations):
             measurement = self.measured_values[i]
             # state update
-            curr_pos = prev_pos + self.alpha*(measurement - prev_pos)
-            curr_vel = prev_vel + self.beta*((measurement - prev_pos) / self.unit_time)
+            curr_pos = prev_pos + (self.alpha*(measurement - prev_pos))
+            curr_vel = prev_vel + (self.beta*((measurement - prev_pos)/self.unit_time))
             # state extrapolation
             next_pos = curr_pos + (self.unit_time * curr_vel)
             next_vel = curr_vel
-            #plot results
-            print(f"iteration: {i}, position: {curr_pos}, velocity: {curr_vel}")
-            plt.plot(i, curr_pos, 'k+')
-            plt.plot(i, curr_vel, 'b*')
-            plt.pause(0.05)
-            plt.draw()
+            #store results
+            positions.append(curr_pos)
+            velocities.append(curr_vel)
             # prepare for next iteration
-            prev_pos = curr_pos
-            prev_vel = curr_vel
-            curr_pos = next_pos
-            curr_vel = next_vel
+            print(prev_pos, prev_vel)
+            prev_pos = next_pos
+            prev_vel = next_vel
 
-model = ConstantVelocityModel(initial_position=30000, initial_velocity=40, unit_time=5, alpha=0.5, beta=0.5)
-model.set_measurements([30110, 30265, 30740, 30750, 31135, 31015, 31180, 31610, 31960, 31865, 32200, 32400, 32600, 32800, 34000])
-model.AB_filter(15)
-time.sleep(5)
+        x = np.arange(0, 10, 1)
+        fig, ax = plt.subplots()
+        ax.plot(positions, color='red')
+        ax.tick_params(axis='y', labelcolor='red')
+        ax2 = ax.twinx()
+        ax2.plot(velocities, color='green')
+        ax2.tick_params(axis='y', labelcolor='green')
+        #ax.plot(x, velocities)
+        plt.show()
+        print(velocities)
+
+model = ConstantVelocityModel(initial_position=30000, initial_velocity=40, unit_time=5, alpha=0.2, beta=0.1)
+model.set_measurements([30110, 30265, 30740, 30750, 31135, 31015, 31180, 31610, 31960, 31865])
+model.AB_filter(10)
